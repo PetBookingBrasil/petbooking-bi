61c86be5f57bfa2f4ecc4eae8aea425ac5f548c4
Patrick Müller <patrick.e.muller@gmail.com>
2017-04-07 16:25:01 -0300

    Fix results for top 3 employees

--- app/controllers/api/v1/employments_controller.rb
+++ app/controllers/api/v1/employments_controller.rb
@@ -1,9 +1,11 @@
 class Api::V1::EmploymentsController < Api::V1::BaseController
   def top_three_employees
     employees = []
-    SalesItem.select('employment_id,
+    SalesItem.joins(:sales_order)
+             .where('sales_orders.aasm_state = 3')
+             .select('employment_id,
                       SUM(coalesce(paid_price, 0)) AS total_paid,
-                      COUNT(id) AS total_services')
+                      COUNT(sales_items.id) AS total_services')
              .group('employment_id')
              .order('total_paid DESC')
              .limit(3).each do |row|
