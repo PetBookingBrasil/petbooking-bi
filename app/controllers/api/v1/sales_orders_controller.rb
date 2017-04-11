e37b863feda00929b410e802dc84b1705fdb180c
Patrick Müller <patrick.e.muller@gmail.com>
2017-04-11 14:45:20 -0300

    Add offline & online sales for today sales

--- app/controllers/api/v1/sales_orders_controller.rb
+++ app/controllers/api/v1/sales_orders_controller.rb
@@ -6,19 +6,33 @@ class Api::V1::SalesOrdersController < Api::V1::BaseController
     start_month = (Date.today - 31.days)
     end_month   = (Date.today - 1.day)
 
-    # Calculate the Budget for today
-    today = SalesOrder.joins(:sales_items)
-                      .paid
-                      .between(start_date, end_date)
-                      .pluck('sales_items.unit_price').sum.to_f
+    # Calculate the Budget for today online sales
+    today_online = SalesOrder.joins(:sales_items)
+                             .paid.online(true)
+                             .between(start_date, end_date)
+                             .pluck('sales_items.unit_price').sum.to_f
 
-    # Calculate the monthly average
-    average = SalesOrder.joins(:sales_items)
-                        .paid
-                        .between(start_month, end_month)
-                        .pluck('sales_items.unit_price').sum.to_f / 30.0
+    # Calculate the monthly average online sales
+    average_online = SalesOrder.joins(:sales_items)
+                               .paid.online(true)
+                               .between(start_month, end_month)
+                               .pluck('sales_items.unit_price').sum.to_f / 30.0
 
-    render json: { today: today, average: average }, status: :ok
+    # Calculate the Budget for today offline sales
+    today_offline = SalesOrder.joins(:sales_items)
+                              .paid.online(false)
+                              .between(start_date, end_date)
+                              .pluck('sales_items.unit_price').sum.to_f
+
+    # Calculate the monthly average offline sales
+    average_offline = SalesOrder.joins(:sales_items)
+                                .paid.online(false)
+                                .between(start_month, end_month)
+                                .pluck('sales_items.unit_price').sum.to_f / 30.0
+
+    render json: { today_online: today_online, average_online: average_online,
+                   today_offline: today_offline, average_offline: average_offline },
+           status: :ok
   end
 
   def total_last_year
